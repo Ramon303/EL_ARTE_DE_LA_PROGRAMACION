f3ef2bc2742b5910b8ed70c71dac07cf1ff44939
Ramon303 <A01385671@tec.mx>
2025-09-17 11:03:58 -0600

    NuevoMundo

--- Actividad_3/pacman.py
+++ Actividad_3/pacman.py
@@ -11,7 +11,6 @@ Exercises
 
 from random import choice
 from turtle import *
-
 from freegames import floor, vector
 
 state = {'score': 0}
@@ -25,6 +24,9 @@ ghosts = [
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
 ]
+
+
+
 # fmt: off
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -49,6 +51,29 @@ tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
 # fmt: on
+
+tiles2 = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0,
+    0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0,
+    0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]
 
 
 def square(x, y):
@@ -87,7 +112,6 @@ def valid(point):
 
     return point.x % 20 == 0 or point.y % 20 == 0
 
-
 def world():
     """Draw world using path."""
     bgcolor('black')
@@ -105,7 +129,6 @@ def world():
                 path.up()
                 path.goto(x + 10, y + 10)
                 path.dot(2, 'white')
-
 
 def move():
     """Move pacman and all ghosts."""
@@ -153,13 +176,115 @@ def move():
     for point, course in ghosts:
         if abs(pacman - point) < 20:
             return
+    
+    if 1 not in tiles:
+        # Cambiar al segundo mundo
+        pacman.x, pacman.y = -40, -80
+        aim.x, aim.y = 5, 0
+        # Reiniciar fantasmas
+        ghosts[0][0].x, ghosts[0][0].y = -180, 160
+        ghosts[1][0].x, ghosts[1][0].y = -180, -160
+        ghosts[2][0].x, ghosts[2][0].y = 100, 160
+        ghosts[3][0].x, ghosts[3][0].y = 100, -160
+
+        world2()   # Dibujar segundo mapa
+        move2()    # Pasar a la función del mundo 2
+        return
 
     ontimer(move, 100)
-
 
 def change(x, y):
     """Change pacman aim if valid."""
     if valid(pacman + vector(x, y)):
+        aim.x = x
+        aim.y = y
+
+
+# David Rangel Monsiváis Mundo nuevo
+def valid2(point):
+    """Return True if point is valid in tiles."""
+    index = offset(point)
+
+    if tiles2[index] == 0:
+        return False
+
+    index = offset(point + 19)
+
+    if tiles2[index] == 0:
+        return False
+
+    return point.x % 20 == 0 or point.y % 20 == 0
+
+def world2():
+    """Draw world using path."""
+    bgcolor('black')
+    path.color('blue')
+
+    for index in range(len(tiles2)):
+        tile = tiles2[index]
+
+        if tile > 0:
+            x = (index % 20) * 20 - 200
+            y = 180 - (index // 20) * 20
+            square(x, y)
+
+            if tile == 1:
+                path.up()
+                path.goto(x + 10, y + 10)
+                path.dot(2, 'white')
+
+def move2():
+    """Move pacman and all ghosts."""
+    writer.undo()
+    writer.write(state['score'])
+
+    clear()
+
+    if valid2(pacman + aim):
+        pacman.move(aim)
+
+    index = offset(pacman)
+
+    if tiles2[ index] == 1:
+        tiles2[index] = 2
+        state['score'] += 1
+        x = (index % 20) * 20 - 200
+        y = 180 - (index // 20) * 20
+        square(x, y)
+
+    up()
+    goto(pacman.x + 10, pacman.y + 10)
+    dot(20, 'yellow')
+
+    for point, course in ghosts:
+        if valid2(point + course):
+                point.move(course)
+        else:
+            options = [
+                vector(5, 0),
+                vector(-5, 0),
+                vector(0, 5),
+                vector(0, -5),
+            ]
+            plan = choice(options)
+            course.x = plan.x
+            course.y = plan.y
+
+        up()
+        goto(point.x + 10, point.y + 10)
+        dot(20, 'red')
+
+    update()
+
+    for point, course in ghosts:
+        if abs(pacman - point) < 20:
+            return
+
+    ontimer(move2, 100)
+
+def change2(x, y):
+    """Change pacman aim if valid."""
+    if valid2(pacman + vector(x, y)):
         aim.x = x
         aim.y = y
 
@@ -178,3 +303,5 @@ onkey(lambda: change(0, -5), 'Down')
 world()
 move()
 done()
+
+
