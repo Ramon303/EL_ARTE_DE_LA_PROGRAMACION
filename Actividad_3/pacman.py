b801b35af4db5ede8906b242529480d1f385468b
Ramon303 <A01385671@tec.mx>
2025-09-17 17:41:32 -0600

    pacman

--- Actividad_3/pacman.py
+++ Actividad_3/pacman.py
@@ -19,10 +19,10 @@ writer = Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+    [vector(-180, 160), vector(10, 0)], #Velocidades (Carlos^2)
+    [vector(-180, -160), vector(0, 10)],
+    [vector(100, 160), vector(0, -10)],
+    [vector(100, -160), vector(-10, 0)],
 ]
 
 
@@ -158,10 +158,10 @@ def move():
             point.move(course)
         else:
             options = [
-                vector(5, 0),
-                vector(-5, 0),
-                vector(0, 5),
-                vector(0, -5),
+                vector(10, 0),
+                vector(-15, 0),
+                vector(0, 10),
+                vector(0, -15),
             ]
             plan = choice(options)
             course.x = plan.x
@@ -261,10 +261,10 @@ def move2():
                 point.move(course)
         else:
             options = [
-                vector(5, 0),
-                vector(-5, 0),
-                vector(0, 5),
-                vector(0, -5),
+                vector(10, 0),
+                vector(-10, 0),
+                vector(0, 10),
+                vector(0, -10),
             ]
             plan = choice(options)
             course.x = plan.x
@@ -302,6 +302,7 @@ onkey(lambda: change(0, 5), 'Up')
 onkey(lambda: change(0, -5), 'Down')
 world()
 move()
+
 done()
 
 
